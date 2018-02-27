4c309868f66627fc2f1c6540d5f772257105e8f2
Artem <jinnytrickz@gmail.com>
2018-02-27 17:36:30 +0300

    problem with adaptive of grid       first slider is ready without hovers

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function () {
+  $('.slider-first').slick({
+    autoplay: false,
+    autoplaySpeed: 1000000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    speed: 1000,
+    prevArrow: '<img src="img/icons/left.png" alt="prev" class="slick-prev">',
+    nextArrow: '<img src="img/icons/right.png" alt="prev" class="slick-next">'
+  });
+});
