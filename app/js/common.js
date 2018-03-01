3d5478c4aa3e81fbb4f1a48f6a66ec40f06e9b0d
Artem <jinnytrickz@gmail.com>
2018-03-01 18:10:36 +0300

    site is finish, start realize adaptive side

--- app/js/common.js
+++ app/js/common.js
@@ -6,9 +6,19 @@ $(document).ready(function () {
     slidesToScroll: 3,
     speed: 1000,
     prevArrow: '<img src="img/icons/left.png" alt="prev" class="slick-prev">',
-    nextArrow: '<img src="img/icons/right.png" alt="prev" class="slick-next">'
+    nextArrow: '<img src="img/icons/right.png" alt="prev" class="slick-next">',
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
   $('.slider-second').slick({
+    arrows: false,
     autoplaySpeed: 1000,
     vertical: true,
     dots: true,
